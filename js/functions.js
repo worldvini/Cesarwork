04a73a1119707e71790b0dd1b8b55a3fa6132a2b
worldvini <worldvini@hotmail.com>
2016-09-14 21:41:34 -0300

    footer
    links in footer

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,13 @@
 		/*popup*/
 		function newPopup(){
-		varWindow = window.open (
-		'mentepositiva.html',
-		'Mente Positiva',
-		"width=350, height=255, top=100, left=110, scrollbars=no ");
+			varWindow = window.open (
+			'mentepositiva.html',
+			'Mente Positiva',
+			"width=360px, height=640px, top=100, left=110, scrollbars=yes ");
 		}
+
+		$(function () {
+        $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
+            	$('.navbar-toggle:visible').click();
+            });
+    	});
